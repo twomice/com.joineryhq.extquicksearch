8dbda00f00d3efdb336082af6839312edc1a8f7f
Allen Shaw <allen@JoineryHQ.com>
2026-03-20 18:21:20 -0500

    civix upgrade, format: 25.10.2

--- extquicksearch.php
+++ extquicksearch.php
@@ -61,15 +61,6 @@ function extquicksearch_civicrm_buildForm($formName, &$form) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function extquicksearch_civicrm_xmlMenu(&$files) {
-  _extquicksearch_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -79,84 +70,12 @@ function extquicksearch_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function extquicksearch_civicrm_postInstall() {
-  _extquicksearch_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function extquicksearch_civicrm_uninstall() {
-  _extquicksearch_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function extquicksearch_civicrm_enable() {
   _extquicksearch_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function extquicksearch_civicrm_disable() {
-  _extquicksearch_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function extquicksearch_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _extquicksearch_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function extquicksearch_civicrm_managed(&$entities) {
-  _extquicksearch_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function extquicksearch_civicrm_caseTypes(&$caseTypes) {
-  _extquicksearch_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function extquicksearch_civicrm_angularModules(&$angularModules) {
-  _extquicksearch_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function extquicksearch_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _extquicksearch_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 function _extquicksearch_get_search_config() {
